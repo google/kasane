2e3f39bb986e6d25e0132cb94468e93c0d7e5955
Vladimir Pouzanov <farcaller@gmail.com>
2018-05-24 17:42:51 +0100

    Made pypi happier about the package description

--- setup.py
+++ setup.py
@@ -12,25 +12,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 def readme():
   with open('README.md') as f:
     return f.read()
 
 setup(name='kasane',
-      version='0.1.1',
+      version='0.1.2',
       description='A simple kubernets deployment manager',
       long_description=readme(),
+      long_description_content_type='text/markdown',
       url='https://github.com/google/kasane',
       author='Vladimir Pouzanov',
       author_email='farcaller@gmail.com',
       keywords='kubernetes helm package-manager docker jsonnet',
       license='Apache-2',
-      packages=[
-        'kasane',
-        'kasane.ops'
-      ],
+      packages=find_packages(),
       install_requires=[
         'click',
         'ruamel.yaml',
